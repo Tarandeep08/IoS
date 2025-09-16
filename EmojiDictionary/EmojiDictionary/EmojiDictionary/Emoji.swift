adf4d483ba40d165a3879fdc1a7172fa7b98ecd9
Tarandeep08 <156651224+Tarandeep08@users.noreply.github.com>
2025-09-16 23:43:40 +0530

    Add files via upload

---
+++ EmojiDictionary/EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Tarandeep singh on 12/09/25.
+//
+
+import Foundation
+
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
